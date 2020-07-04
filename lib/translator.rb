212d8d28532ca7e58c8897a4a0bf5c5340b17420
Jennifer Yoo <jennifer.yoo7@gmail.com>
2020-07-04 16:22:56 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,31 @@
-# require modules here
+require 'pry'
 
-def load_library
-  # code goes here
+require "yaml"
+
+def load_library(file)
+  emoticons = YAML.load_file(file)
+  new_hash = {}
+  emoticons.each do |name, arrays|
+    if !new_hash[name]
+      new_hash[name] = {}
+    end
+      new_hash[name][:english] = arrays[0]
+      new_hash[name][:japanese] = arrays[1]
+  end
+  new_hash
 end
 
-def get_japanese_emoticon
-  # code goes here
+
+def get_japanese_emoticon(file, emoticon)
+  emotion = load_library(file)
+  emotion.each do |emotion_list|
+    emotion_list.each do |emotion_name|
+      emotion_name.each do |english|
+
+    binding.pry
+      end
+    end
+  end
 end
 
 def get_english_meaning
