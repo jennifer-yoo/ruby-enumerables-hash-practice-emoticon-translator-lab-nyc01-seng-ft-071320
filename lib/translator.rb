db1cf08909272814abd275667e774766b769d7a6
Jennifer Yoo <jennifer.yoo7@gmail.com>
2020-07-04 16:36:32 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -24,7 +24,7 @@ def get_japanese_emoticon(file, e_emoticon)
     emotion.each do |emotion_list, lang|
       lang.each do |english, emoticon|
         if emoticon == e_emoticon
-          eng_meaning = emotion
+          eng_meaning = emoticon
         end
       end
       if eng_meaning == ""
